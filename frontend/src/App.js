443363b55cf5932e6c00dcb74eb4c51c23c50390
vedantbhatotia <vedantbhatotia@gmail.com>
2024-05-07 23:10:46 +0530

    signup and login in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
-
 import {Route,Routes} from "react-router-dom"
 import './App.css';
 import Home from './pages/home';
 import Chat from './pages/chat';
 function App() {
   return (
-    <>
+    <div className="App">
     <Routes>
     <Route path = "/" element={<Home></Home>}></Route>
     <Route path ="/chats" element={<Chat></Chat>}></Route>
     </Routes>
-    </>
+    </div>
   );
 }
 
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,41 @@
-const Home = ()=>{
-    return(
-        <>
-        <div>
-            HOME
-        </div>
-        </>
-    )
-}
+import { Container, Box, Text, TabList, Tab, Tabs, TabPanel, TabPanels } from "@chakra-ui/react";
+import Login from "../components/login";
+import Signup from "../components/signup";
+const Home = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work Sans">
+          CHATTING ENGINE
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab >Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
 export default Home;
