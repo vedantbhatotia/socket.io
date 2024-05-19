5b4ce9a38c2e7ac47184eb23d768f65d15fdaa48
vedantbhatotia <vedantbhatotia@gmail.com>
2024-05-19 23:20:01 +0530

    name

--- frontend/src/components/sidedrawer.js
+++ frontend/src/components/sidedrawer.js
@@ -108,7 +108,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
+          Chat-engine
         </Text>
         <div>
           <Menu>
